b112714443f24a8c43c6a1f1368bcf11b7084281
pnellesen <pnellesen@hotmail.com>
2018-06-20 15:28:23 +0000

    Set up state and various functions.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+/*
 const value1 = Math.floor(Math.random() * 100);
 const value2 = Math.floor(Math.random() * 100);
 const value3 = Math.floor(Math.random() * 100);
 const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
 const numQuestions = 0;
 const numCorrect = 0;
-
+*/
 class App extends Component {
+  state = {
+  	value1 : Math.floor(Math.random() * 100),
+	value2 : Math.floor(Math.random() * 100),
+	value3 : Math.floor(Math.random() * 100),
+	numQuestions : 0,
+	numCorrect : 0
+  }
+  createNewQuestion() {
+  	this.setState((prevState) => ({
+  		value1 : Math.floor(Math.random() * 100),
+		value2 : Math.floor(Math.random() * 100),
+		value3 : Math.floor(Math.random() * 100),
+    	numQuestions: prevState.numQuestion++
+  	}))
+  }
+  checkAnswer(answer) {
+    
+  	if (true) {
+    	this.setState((prevState) => ({
+        	numCorrect: prevState.numCorrect++
+        }))
+    }
+    this.createNewQuestion()
+  }
   render() {
+  	let proposedAnswer = Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3;
     return (
       <div className="App">
         <header className="App-header">
@@ -20,12 +45,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${proposedAnswer}`}</p>
           </div>
           <button>True</button>
           <button>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
